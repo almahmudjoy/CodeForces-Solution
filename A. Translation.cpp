6906257c2394661bc347ed26123e575d42a85ac7
Abdullah Al Mahmud Joy <110623413+almahmudjoy@users.noreply.github.com>
2022-11-30 23:44:19 +0600

    Add files via upload

---
+++ A. Translation.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string a,b;
+    cin>>a;
+    cin>>b;
+    reverse(a.begin(), a.end());
+    if(a==b)
+            {cout<<"YES"<<endl;
+
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
