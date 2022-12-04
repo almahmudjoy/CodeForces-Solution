c82c821549b27fd2c877965258de63cc051a2301
Abdullah Al Mahmud Joy <110623413+almahmudjoy@users.noreply.github.com>
2022-12-04 22:18:02 +0600

    Add files via upload

---
+++ A. Ultra-Fast Mathematician.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string  a,b;
+    cin>>a>>b;
+for(size_t i=0; i<a.length(); ++i) //size_t
+{
+       if(a[i]==b[i])
+    {
+      a[i]='0';
+
+    }
+    else
+    {
+      a[i]='1';
+    }
+    cout<<a[i];
+}
+
+
+}
