149de02ce77eb2cfbf6bbd506776a30c6440a5de
Abdullah Al Mahmud Joy <110623413+almahmudjoy@users.noreply.github.com>
2023-02-01 01:17:48 +0600

    Add files via upload

---
+++ A. George and Accommodation.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,c=0;
+    cin>>n;
+    while(n--)
+    {
+        int p,q;
+        cin>>p>>q;
+        if(q-p>=2)
+        {
+           c++;
+        }
+
+    }
+    cout<<c<<endl;
+}
