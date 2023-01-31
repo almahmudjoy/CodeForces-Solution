38390a1a341ed429d6b3b97ee0d35f372044b25b
Abdullah Al Mahmud Joy <110623413+almahmudjoy@users.noreply.github.com>
2023-02-01 01:18:53 +0600

    Add files via upload

---
+++ A. Even Odds.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long n,k,c;
+    cin>>n>>k;
+        if (n% 2 == 0)
+        {
+            c=n/2;
+        }
+        else
+        {
+            c=(n+1)/2;
+        }
+
+        if (k<=c )
+           {
+               cout<<(k*2)-1;
+           }
+           else{cout<<(k-c)*2;
+           }
+
+}
+
