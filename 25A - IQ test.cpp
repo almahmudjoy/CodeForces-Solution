85d94ca1031903a6a22bb2c1691ec80f62e01374
Abdullah Al Mahmud Joy <110623413+almahmudjoy@users.noreply.github.com>
2023-05-01 01:22:52 +0600

    Add files via upload

---
+++ 25A - IQ test.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n,even=0,odd=0;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+        if(arr[i]%2==0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+    }
+    if(even>odd)
+    {
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i]%2!=0)
+            {
+                cout<<i+1;
+                break;
+            }
+        }
+    }
+    else
+    {
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i]%2==0)
+            {
+                cout<<i+1;
+                break;
+            }
+        }
+    }
+}
