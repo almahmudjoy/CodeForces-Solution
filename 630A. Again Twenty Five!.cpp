99efd3c7ecee5e5ba1e853cdc29a80cf580b50fc
Abdullah Al Mahmud Joy <110623413+almahmudjoy@users.noreply.github.com>
2023-06-21 23:23:24 +0600

    Add files via upload

---
+++ 630A. Again Twenty Five!.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long n;
+    cin>>n;
+    long long x=pow(5,n);
+    long long y=abs(x%100);
+    cout<<25<<endl;
+
+}
