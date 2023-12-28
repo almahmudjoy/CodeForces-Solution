439d76894b9b81d3669152e3029776641b22824f
Abdullah Al Mahmud Joy <110623413+almahmudjoy@users.noreply.github.com>
2023-12-28 23:33:10 +0600

    Add files via upload

---
+++ 732A - Buy a Shovel.cpp
@@ -0,0 +1,210 @@
+///   ***   ---   |      Bismillahir Rahmanir Raheem        |||   ---   ***   ///
+///   ***   ---   ||  In the name of ALLAH,Most Gracious,Most Merciful        |||   ---   ***   ///
+\
+
+/*
+                                    ABDULLAH AL MAHMUD JOY
+                        BANGLADESH UNIVERSITY OF BUSINESS AND TECHNOLOGY
+
+
+
+
+
+
+
+
+                               ABDULL
+                                    ABDULLA
+                                     ABDULLAH     .
+                                      ABDULLAHA
+                                      ABDULLAHALL
+                                       ABDULLAHALLM  J
+                                       ABDULLAHALLMA  O
+                                        ABDULLAHALLMA  Y
+                                        ABDULLAHALLMA:
+                                        ABDULLAHALLMAHM
+              .JOYJOYJOYJOYJOY           ABDULLAHALLMAHM J
+        JOYJOYJOYJOYJOYJOYJOYJOYJOY     JOYJOYJOYJOYJOYM
+          JOYJOYJOYJOYJOYJOYJOYJOYJOYJO.  JOYJOYJOYJOYJOYM.M
+            JOYJOYJOYJOYJOYJOYJOYJOYJOY  JOYJOYJOYJOYJOYM
+              JOYJOYJOYJOYJOYJOYJOYJOYJOYJOYJOYJOYJOYJOYJOYM
+                oJOYJOYJOYJOYJOYJOYJOYJOYJOYJOYJOYJOYJOYJOYMMM
+                  JOYJOYJOYJOYJOYJOYJOYJOYJOYJOYJOYJOYJOYJOYMMMMM
+                    JOYJOYJOYJOYJOYJOYJOYJOYJOYJOYJOYJOYJOYJOYMMMMM:                     H
+                     JOYJOYJOYJOYJOYJOYJOYMMMMMMMMMMMMMMMMMMMMMMMMMMM                  .         MMM
+                      MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM              M       MMMMMM
+                       .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM          M   MMMMMMMMMM
+                MM.      MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM       M MMMMMMMMMMMM
+                    MM    MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM    .MMMMMMMMMMMMMM
+                      MM  MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+                        MM MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+               .MMMMMMMMM MMMMMMMMMMMMMMMMMMMMMMMM.MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+                  HMMMMMMMMMMMMMMMMMMMMM.MMMMMMMMM.MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+                     MMMMMMMMMMMMMMM JOY.oMMMMMMM..MMMMMMMMM:MMMMMMMMMMMMMMMMMMMMMMM
+                       MMMMJOYMJOYJOY MM..MMMMMMM...JOYMJOY. MMMMMMMMMMMMMMMMMMMMM
+                         JOYMMMMMMMMMJOY ..MMMMMM...MMMMMM ..MMMMMMMMMMMMMMMMMMM
+                          MMMMMMM:M.JOY.M.. MMMMM M..MMMMM...MMMMMMMMMMMMMMMMMM  JOY
+                            MMMM. .M..MM.M...MMMMMM..MMMMM.. MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM .
+                             MMMM..M....M.....:JOY .JOYMMM..MMMMMMM...MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+                              JOY.M.. ...M......MM.JOYMM.......JOY.M  .MMMMMMMMMMMMMMMMMMMMMMMMM
+                         MMMMMMMM..MM. . JOY.....JOYJOY.M.....M ..MM..M MMMMMMMMMMMMMMMMMMM
+                            .MMMMMHMM. ..MMMM. JOY............o..... . .MMMMMMMMMMMMMMM
+                               JOY. M... .........................M..:.MMMMMMMMMMMM
+                                 oJOY............ .................M.M.MMMMMMMMM
+                                    .....MM........................ . MMMMMM
+                                   M.....M.....................o.MM.MMMMMMMM.
+                                    M........................M.. ...MMMMMMMMMMMMMo
+                                      :....JOY..............JOY..oMMMMMMM
+                                       M...JOY.............MMMMMMM
+                                          .............:MMMMMMMM
+                                          M..... JOY.....M
+                                          M M.............
+                                          ................M
+                                       ooM.................MM  MoMMMMMoooM
+                                  MMoooM......................MoooooooH..oMM
+                              MHooooMoM.....................MMooooooM........M
+                            oooooooMoooM......... o........MoooooooM............
+                            Mooooooooooo.......M.........Moooooooo:..............M
+                           MooMoooooooooM...M........:Mooooooooooo:..............M
+                          M..oooooooooooo .........Mooooooooooooooo..............M
+                         M...Mooo:oooooooo.M....ooooooooooooooooooo..M...........M
+                          ...oooooMoooooooM..Mooooooooooooo:oooooooM.M...........M.
+                         M...ooooooMoo:ooooMoooooooooooooHoooooooooH:M. ...........:
+                         M..MoooooooMoooooooooooooooooo:ooooooMooooMoM..............M
+                         M..ooooooooooMooooooooooooooHoooooooMooHooooM...............M
+                         ...ooooooooooooooooooo:MooooooooooooooMoMoooM................
+                        M...oooooooooooooooooooooooooooooooooooooMooMM................M
+                        ...MooooooooooooooooooooooooooooooooooooooooMo ................
+                        ...MooooooooooooooooooooooooooooooooooooooooM M................M
+                       M...ooooooooooooooooooooooooooooooooooooooooM   ................M
+                       ...MoooooooooooooooooooooooooooooooooooooooMM   .:...............
+                       .....MooooooooooooooooooooooooooooooooooooMoo       .............M
+                       M...... ooooooooooooooooooooooooooooooooooooM       M..............M
+                       M........MooooMMM MM MM  MMMMMMMMMooooooooM         M...............M
+                       .........HM     M:  MM :MMMMMM          M           M...............
+                      M..........M     M   MoM M                           M................M
+                      M.........:M  MoH  M M M MooooHoooMM.   M             M...............M
+                      M..........Moooo MMooM    oooooMooooooooM              M..............H
+                      M.........MooooM  Mooo  : ooooooMooooMoooM              M........ . .o.M
+                      H..  .....ooooo   oooo  M MooooooooooooooM               M... MMMMMMMMMMM
+                      MMMMMMMMMMooooM M oooo  .  ooooooMooooooooM              .MMMMMMMMMMMMMMM
+                      MMMMMMMMMMooooH : ooooH    oooooooooooooooo               MMMMMMMMMMMMMMM
+                      MMMMMMMMMMoooo    ooooM    Moooooooooooooooo              .MMMMMMMMMMMMMMM
+                      MMMMMMMMMMoooo    ooooM    MooooooooooooooooM              MMMMMMMMMMMMMMM
+                      MMMMMMMMMMoooM    ooooM     ooooooooooooooooo               MMMMMMMMMMM:M
+                      MMMMMMMMMMoooM   MooooM     oooooooooooMoooooo               MH...........
+                       . ......Mooo.   MooooM     oooooooooooooooooo              M............M
+                      M.M......oooo    MooooM     Moooooooooooooooooo:           .........M.....
+                      M.M.....Moooo    MooooM      ooooooooooooooooooM            .M............
+                      .......MooooH    MooooM      oooooooooMoooooooooo          M..o...M..o....M
+                      .o....HMooooM    MooooH      MooooooooMooooooooooM          .:M...M.......M
+                     M..M.....MoooM    :oooo:    .MooooooooHooMoooooooooM         M M... ..oM.M
+                      M...M.:.Mooo. MMMMooooo   oooooooooooMoooooooooooooM          ....M. M
+                       M:M..o.Moooooooooooooo MooooooooooooooMooooooooooooM          .Mo
+                              MooooooooooooooMooooooooooooMoMoooooooooooooo
+                              Mooooooooooooooo:ooooooooooooooooooooooooooooo
+                              ooooooooooooooooMooooooooooMoooooooooooooooooo
+                              ooooooooooooooooMoooooooooooMooooooooooooooooHo
+                              ooMooooooooooooooMoooooooooooooooooooooooooooMoM
+                             MooMoooooooooooooo.ooooooooooooooooooooooooooo:oM
+                             MoooooooooooooooooooooooooooooooooooooooooooooooM
+                             MoooMooooooooooooooMooooooooooooooooooooooooooooo.
+                             MoooMooooooooooooooMoooooooooooooooooooooooooMooooM
+                             MooooooooooooooooooMoooooooooooooooooooooooooMoooooM
+                             MooooMoooooooooooooMoooooooooooooooooooooooooMoHooooM
+                             ooooooMooooooooooooooooooooooooooooooooooooooooMoMoooM
+                            MooooooooooooooooooooMooooooooooooooooooooooooooMoooooH:
+                            MoooooooMooooooooooooMoooooooooooooooooooooooooooooHoooM
+                            MooooooooMoooooooooooMoooooooooooooooooooooooooMoooMooooM
+                            Moooooooooooooooooooooooooooooooooooooooooooooo.oooMooooo
+                            MoooooooooooooooooooooooooooooooooooooooooooooMoooooooooM
+                             MooooooooooooooooooooMoooooooooooooooooooooooooooooooooM
+                              MooooooooooooooooooooMHooooooooooooooooooooMoooo:ooooo
+                               MMooooooooooooooooooMoMHoooooooooooooooooooooooMooooo
+                                MMoooooooooooooooMMooo MMooooooooooooooooooooooooooM
+                                MMMoooooooooooooMooooo  oooooooooooooooooooooMooooo
+                                MooMMoooooooooMoooMMoM  ooooHooooooooooooooooMooooM
+                                MooooMooooooMooooMoooM  MoooooMoooooooooooooMooooo
+                                ooooooMMooooooooMooooM  MoooooooooMooooooooooooooM
+                                HooooooMoooooooMooooM    HoooooooHooMooooooooooooo
+                                 oooMoooooooooHoooM         MoooooooooMoooooooooM
+                                  HooooooooooooHM             MooooooooMMoooooooM
+                                   MMMMMMMMMMMMMM                Moooooo:MooooHMM
+                                    MMMMMMM: ...                  MMMMMMMMMMMMMM
+                                   M............M                  MMMMMMMMM ....
+                                   M.MM..........                  M.............M
+                                M ..............MM                 M..............
+                             MMMMM............MMMM                 ..MMMMMMMM ....M
+                           MMMMMMMMMMMMMMMMMMMMMMMM               MMMMMMMMMMMMM...M
+                        .MMMMMMMMMMMMMMMMMMMMMMMMMM               MMMMMMMMMMMMMMMMMM
+                        MMMMMMMMMMMMMMMMMMMMMMMMM                MMMMMMMMMMMMMMMMMMM
+                        :MMMMMMMMMMMMMMMMMMH                     MMMMMMMMMMMMMMMMMMM
+                                                                MMMMMMMMMMMMMMMMMM
+                                                                 MMMMMMMMMMMMMMM
+                                                                  HMMMMMM
+                                                                                                    */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define endl '\n'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu () {            cerr << endl;}
+template < typename T, typename ... hello>void faltu( T arg, const hello &... rest) {cerr << arg << ' ';faltu(rest...);}
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+
+int main()
+{
+    optimize();
+    int k,r;
+    cin>>k>>r;
+
+    int sum=0;
+    for(int i=1;;i++)
+    {
+        sum+=k;
+        if((sum%10==0) || (sum%10==r))
+           {
+               cout<<i; break;
+           }
+           else
+            continue;
+    }
+
+    return 0;
+}
